46e7bd3411231be3e367ad02046fdd1591352521
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-14 15:15:37 +0100

    Updated main pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>Hello, I am about the about page</h1>
+  <h1>Welcome to about page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!</p>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>Hello World</h1>
-  <p>home page</p>
+  <h1>Hello World - welcome home</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias animi suscipit error numquam ea ratione? Fugit nesciunt voluptatibus iste saepe sunt dolor suscipit doloribus eius molestias debitis, accusantium commodi rem illo dolores velit voluptates corrupti sapiente qui. Dignissimos est et laboriosam quisquam enim expedita obcaecati, ducimus quod, maxime, at minus reiciendis quasi ipsum totam voluptates officia fugiat reprehenderit accusantium sunt? Ducimus voluptate suscipit repellat voluptates reiciendis voluptatibus amet hic labore ratione fugiat reprehenderit quaerat quasi odit tempore inventore consequuntur minus molestias, laborum maxime? Adipisci iure minus soluta vitae aspernatur quod sunt commodi, obcaecati temporibus quis inventore consectetur asperiores corrupti!</p>
 </template>
 
 <script>
+
 export default {
+    name: 'home',
+    components: {
+
+    }
 
 }
 </script>
