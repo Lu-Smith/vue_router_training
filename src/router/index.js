736d8c380dd86f9c23d6ac0189aa786a44d82be5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-14 18:25:45 +0100

    Updated NotFound path

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobsDetails from '../views/jobs/JobsDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -31,6 +32,13 @@ const routes = [
   {
     path: '/all-jobs',
     redirect: '/jobs'
+  },
+
+  // catch all 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,13 +2,3 @@
   <h2>404</h2>
   <h3>Page not found</h3>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
